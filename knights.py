011b81d61c40939e21fc6597a35588aa9a6f96b4
Soapy <118913987+soapy070@users.noreply.github.com>
2023-02-23 19:50:10 +0000

    Update knights.py

--- knights.py
+++ knights.py
@@ -131,7 +131,7 @@ def menu(knights, weapons):
                     print(f"Wait... You have no knights! Have a number: {random.randint(0,100)}\n")
             else:
                     print("\n")
-            except:
+        except:
             print("--- Try again! ---\n")
         
 # Required for catching an integer
@@ -149,4 +149,4 @@ def menu(knights, weapons):
             weapons = []
 
 # Run the program
-            menu(knights, weapons)
+        menu(knights, weapons)
